d3d0943061b364ca0bc5477293e69a0109a41d22
Mitchell Underwood <mitchellmunderwood@gmail.com>
2020-09-15 18:10:58 -0400

    Updated API-routes to get workout posting functioning

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -13,16 +13,6 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
-router.get("/api/workouts/:id", ({ body }, res) => {
-  Workout.updated({ _id: req.params.id }, { $push: { exercises: [body] } })
-    .then((dbExercise) => {
-      res.json(dbExercise);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
-
 router.get("/api/workouts/range", (req, res) => {
   Workout.find({})
     .then((dbWorkout) => {
@@ -33,8 +23,18 @@ router.get("/api/workouts/range", (req, res) => {
     });
 });
 
-router.post("/api/workouts/", ({ body }, res) => {
-  Workout.create(body)
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.update({ _id: req.params.id }, { $push: { exercises: [req.body] } })
+    .then((dbExercise) => {
+      res.json(dbExercise);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/api/workouts/", (req, res) => {
+  Workout.create(req.body)
     .then((dbWorkout) => {
       res.json(dbWorkout);
     })
