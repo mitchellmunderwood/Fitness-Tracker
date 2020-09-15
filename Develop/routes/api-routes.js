929d121ea54b018c5268c8471e818fd8bf324663
Mitchell Underwood <mitchellmunderwood@gmail.com>
2020-09-15 17:59:19 -0400

    Created api-routes for data flow on backend

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,31 +1,42 @@
+// const express = require("express");
+const mongoose = "mongoose";
 const router = require("express").Router();
-const Transaction = require("../models/Workout.js");
+const Workout = require("../models/Workout.js");
 
-router.post("/api/transaction", ({ body }, res) => {
-  Transaction.create(body)
-    .then((dbTransaction) => {
-      res.json(dbTransaction);
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
     })
     .catch((err) => {
       res.status(400).json(err);
     });
 });
 
-router.post("/api/transaction/bulk", ({ body }, res) => {
-  Transaction.insertMany(body)
-    .then((dbTransaction) => {
-      res.json(dbTransaction);
+router.get("/api/workouts/:id", ({ body }, res) => {
+  Workout.updated({ _id: req.params.id }, { $push: { exercises: [body] } })
+    .then((dbExercise) => {
+      res.json(dbExercise);
     })
     .catch((err) => {
       res.status(400).json(err);
     });
 });
 
-router.get("/api/transaction", (req, res) => {
-  Transaction.find({})
-    .sort({ date: -1 })
-    .then((dbTransaction) => {
-      res.json(dbTransaction);
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/api/workouts/", ({ body }, res) => {
+  Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
     })
     .catch((err) => {
       res.status(400).json(err);
